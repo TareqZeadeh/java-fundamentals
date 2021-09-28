83081e610de3986e7681a4b2ccd686e9ea38d363
Tareq Zeadeh <tareq.ha130@yahoo.com>
2021-09-28 23:26:31 +0300

    lab03

--- basiclibrary/lib/src/test/java/MainTest.java
+++ basiclibrary/lib/src/test/java/MainTest.java
@@ -48,4 +48,39 @@ public class MainTest {
     }
 
 
+    @Test
+    @DisplayName("minMaxTemp should return high and low temps")
+    public void minMaxTemp(){
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57},
+
+        };
+
+        String highAndMin="High Temperature is 72 and Low Temperature is 51";
+
+        assertEquals(highAndMin,Library.minMaxTemp(weeklyMonthTemperatures));
+
+    }
+    @Test
+    @DisplayName("minMaxTemp should return high and low temps")
+    public void tally(){
+        List<String> votes =new ArrayList<String>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+
+        String nameVote="Bush";
+        assertEquals(nameVote,Library.tally(votes));
+
+    }
+
 }
